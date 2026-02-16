b17b9089272fe890cc733d49b0d29c4835b45313
NicolaStradiotto96 <stradiottonicola@hotmail.it>
2026-02-16 13:29:32 +0100

    Graphic improvements and fixed navbar active items logic and style

--- js/layout.js
+++ js/layout.js
@@ -8,7 +8,7 @@ if (navbarPlaceholder) {
             <nav class="navbar navbar-expand-lg bg-p position-fixed top-0 w-100 z-3 navbar-dark ">
                 <div class="nav-wrapper w-100 mx-auto d-flex align-items-center justify-content-between px-3">
                     <div>
-                        <button class="navbar-toggler me-2 ps-0" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar">
+                        <button class="navbar-toggler me-2 ps-0" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
                             <i class="bi bi-list align-middle display-3 color-s"></i>
                         </button>
                         <a class="navbar-brand m-0" href="index.html">
@@ -21,7 +21,7 @@ if (navbarPlaceholder) {
                                 <a class="nav-link" aria-current="page" href="index.html">Home</a>
                             </li>
                             <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                                <a class="nav-link dropdown-toggle" href="#categories" role="button" data-bs-toggle="dropdown"
                                     aria-expanded="false">
                                     Categories
                                     <span class="dropdown-arrow fw-normal">></span>
@@ -44,13 +44,13 @@ if (navbarPlaceholder) {
                     </div>
                 </div>
             </nav>
-            <div class="offcanvas offcanvas-start" id="offcanvasNavbar">
+            <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbar">
                 <div class="offcanvas-body bg-p">
                     <div class="container bg-p2 rounded">
                         <ul class="navbar-nav fs-5 fw-bold">
                             <hr class="mt-0">
                             <li class="nav-item text-center d-flex justify-content-center">
-                                <a class="nav-link active w-50" aria-current="page" href="index.html">Home</a>
+                                <a class="nav-link w-50" aria-current="page" href="index.html">Home</a>
                             </li>
                             <hr class="color-s">
                             <li class="nav-item dropdown text-center d-flex flex-column align-items-center">
@@ -72,42 +72,43 @@ if (navbarPlaceholder) {
             </div>
         `;
 
-    const currentPath = window.location.pathname;
-    const navLinks = navbarPlaceholder.querySelectorAll('.nav-link');
-
-    navLinks.forEach(link => {
-        const linkHref = link.getAttribute('href');
-        if ((currentPath === "/" || currentPath.endsWith("index.html")) && linkHref === "index.html") {
-            link.classList.add('active');
-        } else if ((currentPath === "/" || currentPath.endsWith("contacts.html")) && linkHref === "contacts.html") {
-            link.classList.add('active');
-        } else if (linkHref === "category.html"){
-            link.classList.add('active');
-        }
-    });
-
+    // NAVBAR CATEGORIES
     fetch("data/cards.json").then((response) => response.json()).then((data) => {
-        const categoriesDropdown = document.querySelectorAll(".categories-dropdown");
+        const categoriesDropdown = navbarPlaceholder.querySelectorAll(".categories-dropdown");
 
         data.forEach((category) => {
             categoriesDropdown.forEach((dropdown) => {
                 const li = document.createElement("li");
-                li.classList.add("nav-link");
+                li.classList.add("nav-item");
                 li.innerHTML = `
-                    <a class="dropdown-item" href="category-${category.url}.html">${category.name}</a>
+                    <a class="dropdown-item nav-link nav-link-dropdown" href="category-${category.url}.html">${category.name}</a>
                 `;
 
                 dropdown.appendChild(li);
             })
         });
+
+        // ACTIVE LINKS
+        const currentPath = window.location.pathname.replace(/\//g, "");
+        const navLinks = navbarPlaceholder.querySelectorAll('.nav-link');
+
+        navLinks.forEach(link => {
+            const linkHref = link.getAttribute('href');
+            if (!linkHref || linkHref === "#" || linkHref === "") return;
+
+            if (currentPath == linkHref) {
+                link.classList.add('active');
+            } else if (currentPath === "" && linkHref === "index.html") {
+                link.classList.add('active');
+            }
+        });
     })
 
-    // NAVBAR
     navbar = navbarPlaceholder.querySelector(".navbar");
 
-    const dropdownToggle = document.querySelector(".dropdown-toggle");
-    const dropdownMenu = document.querySelector('.dropdown-menu');
-    const dropdownArrow = document.querySelector(".dropdown-arrow");
+    const dropdownToggle = navbarPlaceholder.querySelector(".dropdown-toggle");
+    const dropdownMenu = navbarPlaceholder.querySelector('.dropdown-menu');
+    const dropdownArrow = navbarPlaceholder.querySelector(".dropdown-arrow");
 
     const dropdownContainer = dropdownToggle.parentElement;
 
@@ -128,13 +129,15 @@ if (navbarPlaceholder) {
     }
 
     // OFFCANVAS NAVBAR
-    const categoriesNavbar = document.querySelector("#categories-navbar");
-    const offcanvasNavbar = document.querySelector("#offcanvasNavbar");
+    const offcanvasNavbar = navbarPlaceholder.querySelector("#offcanvasNavbar");
 
-    categoriesNavbar.addEventListener("click", () => {
-        const bsOffcanvas = bootstrap.Offcanvas.getOrCreateInstance(offcanvasNavbar);
-        bsOffcanvas.hide();
-    })
+    if (offcanvasNavbar) {
+        const categoriesNavbar = navbarPlaceholder.querySelector("#categories-navbar");
+
+        categoriesNavbar.addEventListener("click", () => {
+            bsOffcanvas.hide();
+        })
+    }
 }
 
 // PROMO BAR
@@ -158,7 +161,7 @@ if (promobarPlaceholder) {
         </div>
     `;
 
-    const btnClose = document.querySelector(".btn-close");
+    const btnClose = promobarPlaceholder.querySelector(".btn-close");
     promoBar = promobarPlaceholder.querySelector(".promo-bar");
 
     if (btnClose && promoBar) {
@@ -169,6 +172,7 @@ if (promobarPlaceholder) {
 }
 
 window.addEventListener("scroll", () => {
+    if (!navbar || !promoBar) return;
 
     if (window.scrollY > 200) {
         navbar.classList.add("nav-scrolled");
@@ -198,13 +202,13 @@ if (footerPlaceholder) {
                     <span class="mb-md-0 color-s">© ${currentYear} Trading Card Website</span>
                 </div>
                 <ul class="nav col-4 list-unstyled d-flex justify-content-end align-items-center">
-                    <li class="footer-icon ms-3">
-                        <a class="color-s" href="#" aria-label="Instagram">
+                    <li>
+                        <a class="color-s" href="#igprofile" aria-label="Instagram">
                             <i class="bi bi-instagram"></i>
                         </a>
                     </li>
-                    <li class="footer-icon ms-3">
-                        <a class="color-s" href="#" aria-label="TikTok">
+                    <li class="ms-3">
+                        <a class="color-s" href="#tiktokprofile" aria-label="TikTok">
                             <i class="bi bi-tiktok"></i>
                         </a>
                     </li>
